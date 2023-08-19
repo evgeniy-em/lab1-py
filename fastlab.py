cdfc8006036d9036611f1e0819fd6343fe3431d4
evgeniyem <dossska.ru@gmail.com>
2023-08-19 11:55:56 +0700

    Prepare for render.com

--- fastlab.py
+++ fastlab.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
@@ -11,7 +10,3 @@ def sum_two_args(x, y):
 @app.get("/")
 def read_root():
     return {"Hello": "world"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
